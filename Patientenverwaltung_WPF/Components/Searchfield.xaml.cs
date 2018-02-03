29bf8793d32eb7035e319bf5ca0af90e6dc1eaa0
JD <john.dederer@protonmail.ch>
2018-02-03 18:02:38 +0100

    Sucen Control und patienten hinzufügen Cotrol aus homeScreen maske

--- Patientenverwaltung_WPF/Components/Searchfield.xaml.cs
+++ Patientenverwaltung_WPF/Components/Searchfield.xaml.cs
@@ -10,26 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Patientenverwaltung_WPF
 {
     /// <summary>
-    /// Interaktionslogik für HomeWindow.xaml
+    /// Interaktionslogik für Searchfield.xaml
     /// </summary>
-    public partial class HomeWindow : Window
+    public partial class Searchfield : UserControl
     {
-        public HomeWindow()
+        public Searchfield()
         {
             InitializeComponent();
-        }
-
-        private void AddPatient_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-
-            }
         }
     }
 }
